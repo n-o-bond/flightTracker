a8d8fbcd3df84f19c812e813beb87a194c3e6120
Natalia <n.o.bondarenko13@gmail.com>
2023-02-28 17:47:31 +0200

    added LoginControllerTest

--- src/test/java/com/example/flighttracker/controller/LoginControllerTest.java
+++ src/test/java/com/example/flighttracker/controller/LoginControllerTest.java
@@ -4,29 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.TestExecutionEvent;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-public class HomeControllerTest {
+public class LoginControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithUserDetails(value = "nora@mail.com", setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    public void homeMethodWithHomeUrl() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+    public void loginMethodWithLoginUrl() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login-form"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
-    }
-    @Test
-    public void homeMethodWithSlashUrl() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.view().name("redirect:/flights/all"));
     }
 }
